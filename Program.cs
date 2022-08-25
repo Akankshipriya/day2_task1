fe7f7022a42ea552ee5874789769762d040e3fc8
Akankshi Priya <akankshipriya24@gmail.com>
2022-08-25 14:07:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            overloading overload = new overloading();
+            overload.DisplayOverload(100);
+            overload.DisplayOverload("method overloading");
+            overload.DisplayOverload("method overloading", 100);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ overloading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading
+{
+    public class overloading
+    {
+        public void DisplayOverload(int a)
+        {
+            System.Console.WriteLine("DisplayOverload " + a);
+        }
+        public void DisplayOverload(string a)
+        {
+            System.Console.WriteLine("DisplayOverload " + a);
+        }
+        public void DisplayOverload(string a, int b)
+        {
+            System.Console.WriteLine("DisplayOverload " + a + b);
+        }
+    }
+}
